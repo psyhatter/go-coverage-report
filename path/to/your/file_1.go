16f4d12f6a8ccc08170a880fdb39181ad0a19f43
Andrey Rusakov <psihatter@gmail.com>
2025-06-16 20:29:18 +0300

    Go code example.

---
+++ path/to/your/file_1.go
@@ -0,0 +1,10 @@
+package your
+
+import (
+	"fmt"
+)
+
+func UncoveredFunction() {
+	// These changes might be covered with tests.
+	fmt.Println("Test me!")
+}
---
+++ path/to/your/file_2.go
@@ -0,0 +1,10 @@
+package your
+
+import (
+	"fmt"
+)
+
+func UncoveredFunction2() {
+	// These changes might be covered with tests.
+	fmt.Println("Test me!")
+}
